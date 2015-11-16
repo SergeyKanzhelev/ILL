2fac15404cdb937ada1c5702e62a762b5e4f2ae0
Sergey Kanzhelev <sergkanz@microsoft.com>
2015-11-16 08:41:28 -0800

    Remove MusicStore in favor of DemoApplication. Make DemoApplicaiton fail

--- DemoApplication/Controllers/ValuesController.cs
+++ DemoApplication/Controllers/ValuesController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace DemoApplication.Controllers
@@ -23,7 +26,47 @@ namespace DemoApplication.Controllers
                 throw new InvalidOperationException();
             }
 
-            return "value";
+            if (id == 2)
+            {
+
+                ThreadStart ts = new ThreadStart(ThreadWorker);
+                Thread thread = new Thread(ts);
+                thread.Start();
+
+
+                Task.Factory.StartNew(() => { throw new Exception(); });
+
+                Task t1 = new Task(() =>
+                {
+                    throw new Exception("Fire-and-forget thread faulted!");
+                });
+                t1.Start();
+
+
+                List<Task> tasks = new List<Task>();
+                var taskFactory = new TaskFactory();
+
+                tasks.Add(taskFactory.StartNew(() =>
+                {
+                    throw new Exception("Thread failed!");
+                }));
+                tasks.Add(taskFactory.StartNew(() =>
+                {
+                    throw new Exception("Yet another thread failed!");
+                }));
+
+                HttpClient client = new HttpClient();
+                client.GetStringAsync("http://localhost:27117/api/values/1");
+            }
+
+            return Process.GetCurrentProcess().Id.ToString();
+        }
+
+
+        void ThreadWorker()
+        {
+            Thread.Sleep(1000);
+            throw new InvalidOperationException();
         }
 
         // POST api/values
