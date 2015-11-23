08b2aa05b4812a6c8f2910a2909a4101b4e0f03b
Sergey Kanzhelev <sergkanz@microsoft.com>
2015-11-22 18:30:26 -0800

    add nuget packages list

--- DemoApplication/Controllers/ValuesController.cs
+++ DemoApplication/Controllers/ValuesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DemoApplication.Models;
+using NuGet;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -12,6 +14,8 @@ namespace DemoApplication.Controllers
 {
     public class ValuesController : ApiController
     {
+        IList<string> NuGetPackages = new List<string>();
+
         // GET api/values/5
         public string Get(int id)
         {
@@ -28,8 +32,50 @@ namespace DemoApplication.Controllers
 
         void ThreadWorker()
         {
-            Thread.Sleep(1000);
-            throw new InvalidOperationException();
+            //ID of the package to be looked up
+            string packageID = "Microsoft";
+            //string packageID = "Microsoft.ApplicationInsights";
+
+            //Connect to the official package repository
+            IPackageRepository repo = PackageRepositoryFactory.Default.CreateRepository("https://packages.nuget.org/api/v2");
+
+            //Get the list of all NuGet packages with ID 'EntityFramework'       
+            //List<IPackage> packages = repo.FindPackagesById("Microsoft").ToList();
+
+            List<IPackage> packages = repo.Search(packageID, true).ToList();
+
+            //Filter the list of packages that are not Release (Stable) versions
+            packages = packages.Where(item => (item.IsReleaseVersion() == false)).ToList();
+
+            PackagesList list = new PackagesList();
+            list.AddPackage += List_AddPackage;
+
+            foreach (IPackage p in packages)
+            {
+                list.Add(p);
+            }
         }
+
+        private static void List_AddPackage(PackagesList self, IPackage package)
+        {
+            Task t1 = new Task(() =>
+            {
+                foreach (var dependencySet in package.DependencySets)
+                {
+                    foreach (var dependency in dependencySet.Dependencies)
+                    {
+                        if (!self.Contains(dependency.Id))
+                        {
+                            IPackageRepository repo = PackageRepositoryFactory.Default.CreateRepository("https://packages.nuget.org/api/v2");
+
+                            self.AddRange(repo.FindPackagesById(dependency.Id));
+                            Console.WriteLine("Add dependency " + dependency.Id + "  for: " + package.Id);
+                        }
+                    }
+                }
+            });
+            t1.Start();
+        }
+
     }
 }
---
+++ DemoApplication/Models/PackagesList.cs
@@ -0,0 +1,45 @@
+﻿using NuGet;
+using System.Collections.Generic;
+
+namespace DemoApplication.Models
+{
+    public class PackagesList
+    {
+        private IList<IPackage> packages = new List<IPackage>();
+
+        public delegate void AddPackageDelegate(PackagesList self, IPackage package);
+
+        public event AddPackageDelegate AddPackage;
+
+        public void Add(IPackage package)
+        {
+            this.packages.Add(package);
+            this.AddPackage(this, package);
+        }
+
+        public void AddRange(IEnumerable<IPackage> packageList)
+        {
+            this.packages.AddRange(packageList);
+            foreach (var p in packageList)
+            {
+                this.AddPackage(this, p);
+            }
+        }
+
+
+        public bool Contains(string id)
+        {
+            bool result = false;
+
+            foreach (var package in this.packages)
+            {
+                if (package.Id == id)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+    }
+}
