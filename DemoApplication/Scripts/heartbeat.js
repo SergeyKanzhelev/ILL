342707a2fd97174a2ee4d6b2d78cdc1a8aef7b7f
Sergey Kanzhelev <sergkanz@microsoft.com>
2015-11-23 14:16:53 -0800

    more cleanup

---
+++ DemoApplication/Scripts/heartbeat.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    setInterval(function () {
+        $.get("/api/heartbeat").done(function (data) {
+            $(heartbeat).text(data);
+        }).fail(function (xhr, error, status) {
+            $(heartbeat).text("failed to connect");
+        });
+    }, 1000);
+});
