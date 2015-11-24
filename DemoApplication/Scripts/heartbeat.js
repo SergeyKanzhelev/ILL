493202ab7b5858bb10d02bd29df5a6c2855fb2de
Sergey Kanzhelev <SergKanz@microsoft.com>
2015-11-23 16:09:17 -0800

    working nice

--- DemoApplication/Scripts/heartbeat.js
+++ DemoApplication/Scripts/heartbeat.js
@@ -1,9 +1,12 @@
 ﻿$(document).ready(function () {
     setInterval(function () {
+        $("#status").attr("class", "glyphicon glyphicon-refresh");
         $.get("/api/heartbeat").done(function (data) {
-            $(heartbeat).text(data);
+            $("#heartbeat").text(data);
+            $("#status").attr("class", "glyphicon glyphicon-ok");
         }).fail(function (xhr, error, status) {
-            $(heartbeat).text("failed to connect");
+            $("#heartbeat").text("failed to connect");
+            $("#status").attr("class", "glyphicon glyphicon-remove");
         });
     }, 1000);
 });
